5371a55b9396518a2cdc35f4965a50908674e32b
maelyg <gauthiem@qut.edu.au>
2022-10-17 15:50:39 +1000

    update thresholds for current evidence categories

--- bin/flag_contamination.py
+++ bin/flag_contamination.py
@@ -91,9 +91,9 @@ def main():
         run_data = run_data.sort_values(["Sample", "Species"], ascending = (True, True))
 
         if diagno == "true":
-            run_data['Evidence_category'] = np.where((run_data['av-pident'] >= 90) & (run_data['PCT_10X'] >= 0.7) & (run_data['length'] >= 45), "KNOWN",
-                                        np.where((run_data['av-pident'] >= 90) & (run_data['PCT_10X'] >= 0.1) & (run_data['PCT_10X'] < 0.7) & (run_data['contig_lenth_max'] >= 45), "KNOWN_FRAGMENT",
-                                        np.where((run_data['av-pident'] < 90) & (run_data['av-pident'] >= 60) & (run_data['PCT_10X'] >= 0.1) & (run_data['length'] >= 45) & (run_data['contig_lenth_max'] >= 200), "CANDIDATE_NOVEL","EXCLUDE")))
+            run_data['Evidence_category'] = np.where((run_data['av-pident'] >= 85) & (run_data['PCT_10X'] >= 0.7) & (run_data['length'] >= 45), "KNOWN",
+                                        np.where((run_data['av-pident'] >= 85) & (run_data['PCT_10X'] >= 0.1) & (run_data['PCT_10X'] < 0.7) & (run_data['length'] >= 45), "KNOWN_FRAGMENT",
+                                        np.where((run_data['av-pident'] < 85) & (run_data['av-pident'] >= 60) & (run_data['PCT_10X'] >= 0.1) & (run_data['length'] >= 45) & (run_data['contig_lenth_max'] >= 200), "CANDIDATE_NOVEL","EXCLUDE")))
             
             targets_df = pd.read_csv(targets, header=0, sep="\t", index_col=None)
             targets_df["Species"] = targets_df["Species"].astype(str)
